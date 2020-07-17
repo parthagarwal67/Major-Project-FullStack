c0d65b493e4232159532de4961da905e9e35be9f
Parth Agarwal <agarwalparth672000@gmail.com>
2020-07-17 14:28:03 +0530

    17july20

--- ProjectHub/src/app/profile/profile.component.ts
+++ ProjectHub/src/app/profile/profile.component.ts
@@ -67,5 +67,14 @@ ngAfterViewInit()
     });
 });
 
+$(".open").on("click", function() {
+  $(".popup-overlay, .popup-content").addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+});
+
 }
 }
